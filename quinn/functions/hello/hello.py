4d590453db149443995512b83d6f6a2b71de81d3
customap <pawar.as@northeastern.edu>
2024-03-20 17:01:24 -0400

    2 functions, 1 layer added

---
+++ quinn/functions/hello/hello.py
@@ -0,0 +1,12 @@
+import json
+import requests
+
+
+def handler(event, context):
+
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
---
+++ quinn/functions/init_chat/init_chat.py
@@ -0,0 +1,55 @@
+from openai import OpenAI
+import os
+import time
+import json
+import boto3
+import logging
+from botocore.exceptions import ClientError
+
+
+openAIClient = OpenAI(
+	api_key=os.environ.get("openai_env"),
+	organization='org-nQnTgti1i8GBtV2AImNwhkCW',
+)
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table('users')
+
+
+def lambda_handler(event, context):
+    try:
+        if "phone_number" in event:
+            phone_number = event["phone_number"]
+            new_thread = openAIClient.beta.threads.create()
+            
+            user = table.get_item(Key={"phone_number": phone_number})
+            item = user['Item']
+            
+            if not "current_thread_id" in item or item["current_thread_id"] == "":
+                response = table.update_item(
+                    Key={"phone_number": phone_number},
+                    UpdateExpression="set current_thread_id=:t",
+                    ExpressionAttributeValues={":t": new_thread.id},
+                    ReturnValues="UPDATED_NEW",
+                )
+                
+                return {
+                    'success': True,
+                    'current_thread_id': response["Attributes"]["current_thread_id"]
+                }
+            else:
+                return {
+                    'success': True,
+                    'current_thread_id': item["current_thread_id"]
+                }
+        else:
+            return {
+                'success': False,
+                'message': "No phone number sent in params"
+            }    
+        
+    except Exception as e:
+        return {
+            'success': False,
+            'message': str(e)
+        }
