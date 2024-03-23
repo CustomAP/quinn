af6c313a6e27d69cbf44043520aeb1ed96cd617f
customap <pawar.as@northeastern.edu>
2024-03-22 20:21:18 -0400

    make summarize_chat extract details and save in db

--- quinn/functions/summarize_chat/summarize_chat.py
+++ quinn/functions/summarize_chat/summarize_chat.py
@@ -15,11 +15,22 @@ openAIClient = OpenAI(
 dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table('users')
 
+def summarize(summary):
+    likes = summary.split("Things the user likes:")[1].split("Things the user hates:")[0].strip()
+    hates = summary.split("Things the user hates:")[1].split("Things the user did today:")[0].strip()
+    did_today = summary.split("Things the user did today:")[1].split("Things the user is going to do today:")[0].strip()
+    going_to_do_today = summary.split("Things the user is going to do today:")[1].split("Things the user is going to do tomorrow or later")[0].strip()
+    going_to_do_later = summary.split("Things the user is going to do tomorrow or later:")[1].split("Things you should avoid talking about with the user:")[0].strip()
+    avoid = summary.split("Things you should avoid talking about with the user:")[1].split("Things you should bring up in your next conversation (cliffhangers):")[0].strip()
+    next_convo = summary.split("Things you should bring up in your next conversation (cliffhangers):")[1].strip()
+    return (likes, hates, did_today, going_to_do_today, going_to_do_later, avoid, next_convo)
+
 def lambda_handler(event, context):
     try:
         if "phone_number" in event:
             phone_number = event["phone_number"]
             
+            #TODO - check if can only get the phone number instead of the object
             user = table.get_item(Key={"phone_number": phone_number})
             item = user['Item']
 
@@ -52,16 +63,27 @@ def lambda_handler(event, context):
                     
             summary_messages = openAIClient.beta.threads.messages.list(thread_id=new_thread.id)
             summary = summary_messages.data[0].content[0].text.value
-            
-            if "summaries" in user:
-                user["summaries"].append(summary)
-            else:
-                user["summaries"] = [summary]
+
+            print("Summary :", summary)
+
+            likes, hates, did_today, going_to_do_today, going_to_do_later, avoid, next_convo = summarize(summary)
+
+            print("summarize: ", summarize(summary))
+
                 
             table.update_item(
                 Key={"phone_number": phone_number},
-                UpdateExpression="set summaries=:s",
-                ExpressionAttributeValues={":s": user["summaries"]}
+                UpdateExpression="set likes=list_append(if_not_exists(likes, :emptylist), :l), hates=list_append(if_not_exists(hates, :emptylist), :h), did_today=list_append(if_not_exists(did_today, :emptylist), :did), going_to_do_today=list_append(if_not_exists(going_to_do_today, :emptylist), :today), going_to_do_later=list_append(if_not_exists(going_to_do_later, :emptylist), :later), avoid=list_append(if_not_exists(avoid, :emptylist), :a), next_convo=list_append(if_not_exists(next_convo, :emptylist), :n)",
+                ExpressionAttributeValues={
+                    ":l": [likes],
+                    ":h": [hates],
+                    ":did": [did_today],
+                    ":today": [going_to_do_today],
+                    ":later": [going_to_do_later],
+                    ":a": [avoid],
+                    ":n": [next_convo],
+                    ":emptylist": []
+                    }
                 # UpdateExpression="set summaries=:s, current_thread_id=:t",
                 # ExpressionAttributeValues={":s": user["summaries"], ":t": ""}
             )
